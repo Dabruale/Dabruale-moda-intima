98e490678470b9660411165c0656eb1305eaa1dc
Dabruale <loja.dabruale.modaintima@gmail.com>
2026-08-20 23:54:12 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -6,8 +6,10 @@ const firebaseConfig = {
     projectId: "dabruale-a9712", 
     storageBucket: "dabruale-a9712.appspot.com", 
     messagingSenderId: "904434904806", 
-    appId: "1:904434904806:web:2054057c62bd41aa361527",
+   appId: "1:904434904806:web:2054057c62bd41aa361527",
+  measurementId: "G-H4GF971P31"
 };
+
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
